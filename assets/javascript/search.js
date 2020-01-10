fea6ec1a77f448a73d7b314202ea11bc6a23ac51
Tim Burns <burnstimothy87@gmail.com>
2020-01-09 20:26:12 -0500

    js css

---
+++ assets/javascript/search.js
@@ -0,0 +1,32 @@
+var lunr = require('lunr');
+
+let documents = [{
+        "name": "Lunr",
+        "text": "Like Solr but much smaller and not as bright",
+    },
+    {
+        "name": "React",
+        "text": "A JavaScript library for building user interfaces,"
+    },
+    {
+        "name": "lodash",
+        "text": "A modern JavaScipt utility library delivering modularity, performance, and extras"
+    },
+    {
+        "name": "Bud Light",
+        "text": "Taste like piss"
+    },
+];
+
+var idx = lunr(function () {
+    this.ref('name')
+    this.field('text')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+})
+
+idx.search("bright")
+
+console.log(documents)
