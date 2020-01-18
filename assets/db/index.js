0839be5932e545029fc188270337508fd24baa52
Tim Burns <burnstimothy87@gmail.com>
2020-01-17 20:39:12 -0500

    FUCK

--- assets/db/index.js
+++ assets/db/index.js
@@ -1,3 +1,43 @@
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../javascript/connection");
+
+// Creates a "Character" model that matches up with DB
+var Beer = sequelize.define("beer", {
+    // the routeName gets saved as a string
+    routeName: Sequelize.STRING,
+    // the name of the character (a string)
+    beer_name: Sequelize.STRING,
+    // the character's role (a string)
+    beer_brewery: Sequelize.STRING,
+    // the character's age (a string)
+    beer_style: Sequelize.INTEGER,
+
+    // and the character's force points (an int)
+    beer_abv: Sequelize.INTEGER
+}, {
+    // disable the modification of tablenames; By default, sequelize will automatically
+    // transform all passed model names (first parameter of define) into plural.
+    // if you don't want that, set the following
+    freezeTableName: true
+});
+
+// Syncs with DB
+Beer.sync();
+
+// Makes the Character Model available for other files (will also create a table)
+module.exports = Beer;
+
+
+
+
+
+
+
 // const mysql = require('mysql');
 
 // mysql.createPool({
